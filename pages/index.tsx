e2c2885d39e01883bf3ff45ad860d522adf60233
Israel <ogasvega.israel@gmail.com>
2022-03-12 23:56:41 -0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@nextui-org/react'
+import type { NextPage } from 'next'
+
+const HomePage: NextPage = () => {
+  return (
+    <>
+      <Button color="gradient">
+        Test
+      </Button>
+    </>
+  )
+}
+
+export default HomePage
